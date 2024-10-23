ce86923a560b0ccc370830193f771da27d081a6a
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-10-23 10:49:44 -0700

    cmp

--- source/conf.py
+++ source/conf.py
@@ -65,7 +65,8 @@ rst_prolog = """
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'toctree_filter'
+    'toctree_filter',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
